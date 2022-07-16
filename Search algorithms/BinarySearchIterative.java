002519ecf04b6cd8d82030b2b6f69b7228e679ea
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-07-16 23:50:55 +0530

    Create BinarySearchIterative.java

---
+++ Search algorithms/BinarySearchIterative.java
@@ -0,0 +1,27 @@
+public class BinarySearchIterative {
+    int binarySearch(int arr[], int x){
+        int l = 0, r = arr.length - 1;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == x)
+                return mid;
+            if (arr[mid] < x)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        // We reach here when element is not present in array
+        return -1;
+    }
+    public static void main(String[] args){
+        BinarySearchIterative ob = new BinarySearchIterative();
+        int arr[] = {2, 3, 4, 10, 40};
+        int n = arr.length;
+        int x = 3;
+        int result = ob.binarySearch(arr, x);
+        if (result == -1)
+            System.out.println("Element is not present in the array.");
+        else
+            System.out.println("Element Index is: " + result);
+    }
+}
