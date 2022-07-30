5a191f7131d85446326246204abd588168feb33c
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-07-30 23:27:38 +0530

    Update SelectionSort.java

--- Sorting algorithms/SelectionSort.java
+++ Sorting algorithms/SelectionSort.java
@@ -17,7 +17,7 @@ class SelectionSort{
     void printArray(int arr[]){
         int n = arr.length;
         for (int i = 0; i < n; i++)
-            System.out.println(arr[i] + " ");
+            System.out.print(arr[i] + " ");
         System.out.println();
     }
 
