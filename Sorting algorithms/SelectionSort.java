bc773ed75c4553ec3f4980c51fe082297ed6fd89
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-07-30 22:50:21 +0530

    Create SelectionSort.java

---
+++ Sorting algorithms/SelectionSort.java
@@ -0,0 +1,31 @@
+class SelectionSort{
+    void sort(int arr[]){
+        int n = arr.length;
+        for (int i = 0; i < n; i++){
+            int min = i;
+            for (int j = i + 1; j < n; j++)
+                if (arr[j] < arr[min])
+                    min = j;
+            //Swap two element
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Helper method to print Array.
+    void printArray(int arr[]){
+        int n = arr.length;
+        for (int i = 0; i < n; i++)
+            System.out.println(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        int arr[] = {75, 99, 23, 6, 1, 100};
+        SelectionSort ob = new SelectionSort();
+        ob.sort(arr);
+        System.out.println("Sorted array is: ");
+        ob.printArray(arr);
+    }
+}
