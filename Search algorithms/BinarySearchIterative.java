4488871743e79cee78c3cc2399f9f2ed978e6b31
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-07-30 18:20:00 +0530

    Update BinarySearchIterative.java

--- Search algorithms/BinarySearchIterative.java
+++ Search algorithms/BinarySearchIterative.java
@@ -3,7 +3,7 @@ public class BinarySearchIterative {
         int l = 0, r = arr.length - 1;
         while(l <= r){
             int mid = l + (r - l) / 2;
-            if (arr[mid] == x)
+            while (arr[mid] == x)
                 return mid;
             if (arr[mid] < x)
                 l = mid + 1;
