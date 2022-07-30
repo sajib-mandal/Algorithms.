60ac9a967c4e96f83d50f213135ab9dd74d97119
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-07-30 18:16:08 +0530

    Update BinarySearchIterative.java

--- Search algorithms/BinarySearchIterative.java
+++ Search algorithms/BinarySearchIterative.java
@@ -25,3 +25,34 @@ public class BinarySearchIterative {
             System.out.println("Element Index is: " + result);
     }
 }
+
+
+
+
+
+
+public class binarySearch {
+    int binary_search(int arr[], int l, int r, int x){
+        while (l <= r){
+            int mid = l + (r - l) / 2;
+            if (mid == x)
+                return mid;
+            if (mid < x)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int arr[] = {2, 3, 4, 10, 40};
+        int n = arr.length;
+        int x = 3;
+        binarySearch ob = new binarySearch();
+        int result = ob.binary_search(arr, 0, n - 1, x);
+        if (result == -1)
+            System.out.println("Not found!");
+        else
+            System.out.println("Element is: " + result);
+    }
+}
